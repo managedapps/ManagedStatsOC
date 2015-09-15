7d5607346943bbd29560eb847d5b8c86fe3d1baa
Bob Pascazio <bob@bytefly.com>
2015-09-15 08:46:57 -0400

    Updates for disclaimer and first time run.

---
+++ Pod/Classes/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  Pods
+//
+//  Created by Bob Pascazio on 9/15/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#define kFirstRunURL        "http://portal.managedapps.co/api/v1/apps/%@/first_run"
+#define kNSUDKeyFirstRun    "first_run"
--- Pod/Classes/ManagedStats.m
+++ Pod/Classes/ManagedStats.m
@@ -8,6 +8,7 @@
 
 #import "ManagedStats.h"
 #import "AFNetworking.h"
+#import "Constants.h"
 
 @implementation ManagedStats {
     
@@ -23,8 +24,33 @@
 
 
 - (void)recordRun {
-    NSLog(@"MANAGEDAPPS.CO -> Recording a Run!");
-    NSLog(@"MANAGEDAPPS.CO -> appkey is %@", _appKey);
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *firstRun = [defaults objectForKey:@kNSUDKeyFirstRun];
+
+    if (firstRun == nil) {
+    
+        NSLog(@"MANAGEDAPPS.CO -> Recording a Run!");
+        NSLog(@"MANAGEDAPPS.CO -> appkey is %@", _appKey);
+        NSString *fullUrl = [NSString stringWithFormat:@kFirstRunURL, _appKey];
+        NSLog(@"MANAGEDAPPS.CO -> fullUrl %@", fullUrl);
+        NSURL *URL = [NSURL URLWithString:fullUrl];
+        NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc]
+                                         initWithRequest:request];
+        operation.responseSerializer = [AFJSONResponseSerializer serializer];
+        [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"MANAGEDAPPS.CO -> result %@", responseObject);
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            [defaults setObject:@"YES" forKey:@kNSUDKeyFirstRun];
+            [defaults synchronize];
+        } failure:nil];
+        [operation start];
+    } else {
+
+        NSLog(@"MANAGEDAPPS.CO -> First Run Previously Recorded.");
+    }
+    
 }
 
 @end
